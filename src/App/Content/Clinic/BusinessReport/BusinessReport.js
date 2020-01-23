0544fea554c310ef41304db1c996b1cd4a48ac36
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-01-23 18:34:01 +0100

    updated BusinessReport page

--- src/App/Content/Clinic/BusinessReport/BusinessReport.js
+++ src/App/Content/Clinic/BusinessReport/BusinessReport.js
@@ -5,6 +5,7 @@ import Header from '../../Header/Header';
 import Footer from '../../Footer/Footer';
 import './BusinessReport.css';
 import axios from 'axios';
+import ReactTable from "react-table";
 
 class BusinessReport extends React.Component {
 
@@ -15,7 +16,12 @@ class BusinessReport extends React.Component {
             id: '',
             name: '',
             avg: '',
-            doctors: []
+            doctors: [{
+                id: '',
+                firstName: '',
+                lastName: '',
+                stars: ''
+            }]
         }
     }
     
@@ -32,23 +38,77 @@ class BusinessReport extends React.Component {
                     address: response.data.address,
                     city: response.data.city
                 })
-            }) .catch((error) => console.log(error))
+            }).then(() => {
+                axios.get("http://localhost:8080/api/clinic-doctors/" + this.state.id)  
+                .then(response => {
+                    let tmpArray = []
+                    for (var i = 0; i < response.data.length; i++) {
+                        tmpArray.push(response.data[i])
+                    }
+            
+                    this.setState({
+                        doctors: tmpArray
+                    })
+                })
+                .catch((error) => console.log(error))
+            }).catch((error) => console.log(error))
         }
 
     render() {
+
+        const columns=[
+            {
+              Header:'Id',
+              id: 'id',
+              accessor: d => d.id,
+              width: 50,
+              filterable: false
+          },{
+            Header:'First Name',
+            accessor: 'firstName'
+          },{
+            Header:'Last Name',
+            accessor: 'lastName'
+          },{
+            Header:'Email Address',
+            accessor: 'username'
+          },{
+              Header: 'Stars',
+              accessor: 'stars',
+              width: 70
+          }]
+
         return(
             
             <div className="BusinessReport">
                 <Header/>
-                <br/>
-                <h3>Business Report <h4><em>{this.state.name}</em></h4></h3>
-                <hr/>
-                <h5>Average grade: {this.state.avg}</h5>
-                <h5>Doctors grades: {this.state.avg}</h5>
-                <h5>Monthly Income: {this.state.avg}</h5>
-                <h5>Graph: {this.state.avg}</h5>
+                <div className="row">
+                    <div className="col-10">
+                        <br/>
+                        <h3>Business Report </h3>
+                        <h3><em>{this.state.name}</em></h3>
+                        <hr/>
+                        <h5>Average grade: {this.state.avg}</h5>
+                        <h5>Monthly Income: {this.state.avg}</h5>
+                        <h5>Graph: {this.state.avg}</h5>
+                        <br/>
+                        <h4>Doctors</h4>
+                        <div className='doc-table'>
+                        <ReactTable 
+                            data={this.state.doctors}
+                            columns={columns}
+                            filterable
+                            onFilteredChange = {this.handleOnFilterInputChange}
+                            defaultPageSize = {5}
+                            pageSizeOptions = {[5, 10, 15]}
+                            />
+                        </div>
+                    </div>
+                    <div className="col-2 busin-report-image">
 
-
+                    </div>
+                </div>
+                
                 <Footer/>
             </div>
 
