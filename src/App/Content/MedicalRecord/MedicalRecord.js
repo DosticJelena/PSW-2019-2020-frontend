58c3a57b0929f7d108785eed3527fd6678e66982
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-02-05 15:00:28 +0100

    updated MedicalRecord page

--- src/App/Content/MedicalRecord/MedicalRecord.js
+++ src/App/Content/MedicalRecord/MedicalRecord.js
@@ -8,6 +8,8 @@ import axios from 'axios';
 import ReactTable from "react-table";
 import { Button } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
+import Modal from '../../UI/Modal/Modal';
+import MedicalHistory from './AppointmentHistory/MedicalHistory/MedicalHistory';
 
 
 class MedicalRecord extends React.Component{
@@ -20,11 +22,30 @@ class MedicalRecord extends React.Component{
            bloodType:'',
            allergies:[],
            history:[],
-           filtered:[]
+           filtered:[],
+           id:'',
+           reportModalHandler:false
         }
     }
 
+    reportModalHandler=(id)=>{
+      this.setState({ id: id, reportModalHandler: true });
+      this.props.history.push("/medical-record/" + id);
+     }
+
+    modalClosedHandler = () => {
+      this.setState({  reportModalHandler: false });
+      this.props.history.push("/medical-record");
+    }
+
+    handleChange(e) {
+      this.setState({ ...this.state, [e.target.name]: String(e.target.value) });
+    }
+
     componentDidMount(){
+
+      this.modalClosedHandler();
+
       var token = localStorage.getItem('token');
       axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
       axios.get("http://localhost:8080/auth/getMyUser")  
@@ -87,6 +108,9 @@ class MedicalRecord extends React.Component{
         return(
 
             <div className="MedicalRecord">
+              <Modal show={this.state.reportModalHandler} modalClosed={this.modalClosedHandler}>
+                <MedicalHistory id={this.state.id} reload={this.state.reportModalHandler} />
+              </Modal>
                 <Header/>
                 <div>               
                 <div className="row">
@@ -107,7 +131,78 @@ class MedicalRecord extends React.Component{
                       </div>
                   </div>
                 </div>
+
+                <div className="col-7">
+                <h3>Previous appointments and medical history</h3>
+                <hr/>
+                </div>
                 <div id='history-appointments-table' className="history-appointments-table rtable">     
+                <ReactTable 
+                        data={this.state.history}
+                        filterable
+                        filtered={this.state.filtered}
+                        onFilteredChange={(filtered, column, value) => {
+                          this.onFilteredChangeCustom(value, column.id || column.accessor);
+                        }}
+                        defaultFilterMethod={(filter, row, column) => {
+                            const id = filter.pivotId || filter.id;
+                            if (typeof filter.value === "object") {
+                              return row[id] !== undefined
+                                ? filter.value.indexOf(row[id]) > -1
+                                : true;
+                            } else {
+                              return row[id] !== undefined
+                                ? String(row[id]).indexOf(filter.value) > -1
+                                : true;
+                            }
+                          }}
+                        columns={[{
+                                    Header: 'Report id',
+                                    accessor: 'id',
+                                },{
+                                    Header: 'Start time',
+                                    accessor: 'startTime',
+                                },
+                                {
+                                    Header: 'End time',
+                                    accessor: 'endTime',
+                                },
+                                {
+                                    Header: 'Doctors name',
+                                    accessor: 'doctorFirstName',
+                                    width:150
+                                }, 
+                                {
+                                    Header: 'Doctors last name',
+                                    accessor: 'doctorLastName',
+                                    width:200
+                                },
+                                {
+                                    Header: 'Appointment type',
+                                    accessor: 'type',
+                                },
+                                {
+                                    Header: 'Specialization',
+                                    accessor: 'specialization',
+                                },
+                                {
+                                  Header: 'Medical history',
+                                  Cell: row => (                        
+                                    <div>
+                                      <Button onClick={() => this.reportModalHandler(row.original.id)}>See details</Button>
+                                    </div>
+                                    ),
+                                    filterable:false
+                                }
+                        ]}
+                        defaultPageSize = {10}
+                    />
+                </div>
+                <div className="col-6">
+                    <h3>Scheduled appointments</h3>
+                    <hr/>
+                </div>
+                <div className="rtable">
                 <ReactTable 
                         data={this.state.history}
                         filterable
@@ -154,14 +249,18 @@ class MedicalRecord extends React.Component{
                                     accessor: 'specialization',
                                 },
                                 {
-                                  Header: 'Diagnosis',
-                                  accessor: 'diagnosis',
-                                  width:500
+                                  Header: '',
+                                  Cell: row => (                        
+                                    <div>
+                                      <Button onClick={() => this.cancelApp(row.original.doctorId)}>Cancel</Button>
+                                    </div>
+                                    ),
+                                    filterable:false
                                 }
                         ]}
                         defaultPageSize = {10}
                     />
-                </div>
+                  </div>  
                 </div>
                 <Footer/>
             </div>
