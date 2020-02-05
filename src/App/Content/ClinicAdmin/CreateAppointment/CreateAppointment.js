71262caa7ed78db78759b28d8aa7b1c3160b88cf
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-02-05 18:53:24 +0100

    added fields to state, options to change doctor and time

--- src/App/Content/ClinicAdmin/CreateAppointment/CreateAppointment.js
+++ src/App/Content/ClinicAdmin/CreateAppointment/CreateAppointment.js
@@ -19,13 +19,29 @@ class CreateAppointment extends React.Component {
             ordination: '',
             doctors: [],
             prices: [],
-            doctor: '',
-            price: ''
+            doctorId: '',
+            price: '',
+            changeDoctor: false,
+            date: '',
+            startTime: '',
+            endTime: '',
+            changeTime: false
         }
     }
 
     handleChange = e => {
-        this.setState({ ...this.state, [e.target.name]: String(e.target.value) });
+
+        if (e.target.name == "doctorId") {
+            for (var i = 0; i < this.state.doctors.length; i++) {
+                if (this.state.doctors[i].id == e.target.value) {
+                    this.setState({ doctorId: this.state.doctors[i].id, doctorFN: this.state.doctors[i].firstName, doctorLN: this.state.doctors[i].lastName });
+                    console.log(this.state);
+                }
+            }
+        } else {
+            this.setState({ ...this.state, [e.target.name]: String(e.target.value) });
+        }
+
     }
 
     createAppointmentRequest = () => {
@@ -51,6 +67,23 @@ class CreateAppointment extends React.Component {
 
     }
 
+    changeDoctorVisible = () => {
+        this.setState({ changeDoctor: true, changeTime: false })
+    }
+
+    changeTimeVisible = () => {
+        this.setState({ changeDoctor: false, changeTime: true })
+    }
+
+    okClose = () => {
+        this.setState({ changeDoctor: false, changeTime: false })
+    }
+
+    handleChangeTime = e => {
+        this.setState({ ...this.state, [e.target.name]: String(e.target.value) });
+        //this.fetchDoctors();
+    }
+
     render() {
 
         console.log(this.props.appReqId)
@@ -70,6 +103,9 @@ class CreateAppointment extends React.Component {
                         appReqId: this.props.appReqId,
                         start: response.data.startDateTime,
                         end: response.data.endDateTime,
+                        startTime: String(response.data.startDateTime).substr(11, 5),
+                        endTime: String(response.data.endDateTime).substr(11, 5),
+                        date: String(response.data.endDateTime).substr(0, 10),
                         patientId: response.data.patientId,
                         type: typee,
                         spec: response.data.typeSpec,
@@ -130,92 +166,64 @@ class CreateAppointment extends React.Component {
                 .catch((error) => console.log(error))
         }
 
-        var pricesByEnum = [];
-        for (var i = 0; i < this.state.prices.length; i++) {
-            if (this.state.prices[i].appointmentEnum == this.state.typeString) {
-                pricesByEnum.push(this.state.prices[i]);
+        var doctorsBySpec = []
+        for (var i = 0; i < this.state.doctors.length; i++) {
+            if (this.state.doctors[i].specialization == this.state.spec) {
+                doctorsBySpec.push(this.state.doctors[i]);
             }
         }
 
-        const steps =
-            [
-                {
-                    name: 'Step 1',
-                    component:
-                        (<div className="stepp">
-                            <hr />
-                            <h5>Choose ordination for the appointment:</h5>
-                            <hr />
-                            <div className="form-row">
-                                <div className="form-group col-md-6">
-                                    <label htmlFor="ordination">Ordination</label>
-                                    <select required className="custom-select mr-sm-2" name="ordination" id="ordination" onChange={this.handleChange} >
-                                        <option defaultValue="0">Choose...</option>
-                                        {this.state.ordinations.map((ord, index) => (
-                                            <option key={ord.id} value={ord.id}>{ord.number}</option>
-                                        ))}
-                                    </select>
-                                </div>
-                            </div>
-                        </div>)
-                },
-                {
-                    name: 'Step 2',
-                    component:
-                        (<div className="stepp">
-                            <hr />
-                            <h5>Choose doctor for the appointment:</h5>
-                            <hr />
-                            <div className="form-row">
-                                <div className="form-group col-md-6"><label htmlFor="doctor">Doctor</label>
-                                    <select required className="custom-select mr-sm-2" name="doctor" id="doctor" onChange={this.handleChange} >
-                                        <option defaultValue="0" >Choose...</option>
-                                        {this.state.doctors.map((doctor, index) => (
-                                            <option key={doctor.id} value={doctor.id}>{doctor.firstName} {doctor.lastName}</option>
-                                        ))}
-                                    </select>
-                                </div>
-                            </div>
-                        </div>)
-                },
-                {
-                    name: 'Step 3',
-                    component:
-                        (<div className="stepp">
-                            <hr />
-                            <h5>Choose price for the appointment:</h5>
-                            <hr />
-                            <div className="form-row">
-                                <div className="form-group col-md-6"><label htmlFor="doctor">Price</label>
-                                    <select required className="custom-select mr-sm-2" name="price" id="price" onChange={this.handleChange} >
-                                        <option defaultValue="0" >Choose...</option>
-                                        {pricesByEnum.map((price, index) => (
-                                            <option key={price.id} value={price.id}>{price.appointmentType} | {price.appointmentPrice}€</option>
-                                        ))}
-                                    </select>
-                                </div>
-                            </div>
-                        </div>)
-                },
-                {
-                    name: 'Step 4',
-                    component:
-                        (<div className="stepp">
-                            <hr />
-                            <h5>Appointment details:</h5>
-                            <hr />
-                            <div>Type: <em>{this.state.typeString} - {this.state.spec}</em></div>
-                            <div>Doctor: <em>{this.state.doctorFN} {this.state.doctorLN}</em></div>
-                            <div>Ordination: <em>{this.state.ordination} </em></div>
-                            <div>Price: <em>{this.state.price}€ </em></div>
-                            <div>Patient: <em>{this.state.patientFN} {this.state.patientLN}</em></div>
-                            <div>Start: <em>{this.state.start}</em></div>
-                            <div>End: <em>{this.state.end}</em></div>
-                            <hr />
-                            <button className="btn calendar-ord" onClick={() => this.createAppointmentRequest()}>Create</button>
-                        </div>)
-                }
-            ]
+        var changeDoctor;
+        if (this.state.changeDoctor == true) {
+            changeDoctor = (<div>
+                <div className="form-row">
+                    <div className="form-group col-8">
+                        <label htmlFor="doctor">Doctor</label>
+                        <select required className="custom-select mr-sm-2" name="doctorId" id="doctorId" onChange={this.handleChange} >
+                            <option defaultValue="0" >Choose...</option>
+                            {doctorsBySpec.map((doctor, index) => (
+                                <option key={doctor.id} value={doctor.id}>{doctor.firstName} {doctor.lastName} ({this.state.spec})</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="form-group col-2">
+                        <label style={{ color: "white" }}>Ok</label>
+                        <br />
+                        <button className="btn calendar-ord" onClick={() => this.okClose()}>Ok</button>
+                    </div>
+                </div>
+            </div>)
+        }
+
+        var changeTime;
+        if (this.state.changeTime == true) {
+            changeTime = (<div>
+                <p style={{ color: "darkcyan" }}>Work time is from 07:00 to 20:00</p>
+                <div className="form-row">
+                    <div className="form-group col-md-5">
+                        <label htmlFor="date">Date</label>
+                        <input required type="date" className="form-control" name="date" id="date" placeholder="Date"
+                            onChange={this.handleChange} />
+                    </div>
+                    <div className="form-group col-md-3">
+                        <label htmlFor="date">Start</label>
+                        <input required type="time" min="07:00" defaultValue={this.state.startTime} className="form-control" name="startTime" id="start" placeholder="Start time"
+                            onChange={this.handleChangeTime} />
+                    </div>
+                    <div className="form-group col-md-3">
+                        <label htmlFor="time">End</label>
+                        <input required type="time" max="20:00" defaultValue={this.state.endTime} className="form-control" name="endTime" id="end" placeholder="End time"
+                            onChange={this.handleChangeTime} />
+                    </div>
+                    <div className="form-group col-1">
+                        <label style={{ color: "white" }}>Ok</label>
+                        <br />
+                        <button className="btn calendar-ord" onClick={() => this.okClose()}>Ok</button>
+                    </div>
+                </div>
+            </div>)
+        }
+
 
         return (
             <div className="CreateAppointment">
@@ -226,14 +234,16 @@ class CreateAppointment extends React.Component {
                 <div className="row">
                     <div className="col-5">
                         <table>
-                            <tr><td>Type: </td><td><em>{this.state.typeString}</em></td></tr>
-                            <tr><td>Specialization: </td><td><em>{this.state.spec}</em></td></tr>
+                            <tr><td>Patient: </td><td><em><strong>{this.state.patientFN} {this.state.patientLN}</strong></em></td></tr>
+                            <tr><td>Type: </td><td><em><strong>{this.state.typeString}</strong></em></td></tr>
+                            <tr><td>Specialization: </td><td><em><strong>{this.state.spec}</strong></em></td></tr>
+                            <tr><td>Price: </td><td><em><strong>{this.state.price}€</strong></em></td></tr>
+                            <br />
                             <tr><td>Doctor: </td><td><em>{this.state.doctorFN} {this.state.doctorLN}</em></td></tr>
+                            <tr><td>Date: </td><td><em>{this.state.date}</em></td></tr>
+                            <tr><td>Start: </td><td><em>{this.state.startTime}</em></td></tr>
+                            <tr><td>End: </td><td><em>{this.state.endTime}</em></td></tr>
                             <tr><td>Ordination: </td><td><em>{this.state.ordination}</em></td></tr>
-                            <tr><td>Price: </td><td><em>{this.state.price}€</em></td></tr>
-                            <tr><td>Patient: </td><td><em>{this.state.patientFN} {this.state.patientLN}</em></td></tr>
-                            <tr><td>Start: </td><td><em>{this.state.start}</em></td></tr>
-                            <tr><td>End: </td><td><em>{this.state.end}</em></td></tr>
                         </table>
                     </div>
                     <div className="col-7">
@@ -248,19 +258,17 @@ class CreateAppointment extends React.Component {
                                 </select>
                             </div>
                         </div>
+                        {changeDoctor}
+                        {changeTime}
                     </div>
                 </div>
                 <hr />
                 <button className="btn create-bttn" onClick={() => this.createAppointmentRequest()}>Create</button>
-                <button className="btn calendar-ord" style={{float:"right", marginLeft: '10px'}}>Change doctor</button>
-                <button className="btn calendar-ord" style={{float:"right", marginLeft: '10px'}}>Change time</button>
+                <button className="btn calendar-ord" onClick={() => this.changeDoctorVisible()} style={{ float: "right", marginLeft: '10px' }}>Change doctor</button>
+                <button className="btn calendar-ord" onClick={() => this.changeTimeVisible()} style={{ float: "right", marginLeft: '10px' }}>Change time</button>
             </div>)
     }
 
 }
 
 export default withRouter(CreateAppointment);
-
-/*/*<div className='step-progress'>
-                    <StepZilla steps={steps} />
-                </div> */
